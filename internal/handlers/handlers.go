b8ac8854ef310fb9f096e1326d2b58496bdfc81d
Chris Myers <chris.myers6@accesscfa.com>
2025-09-03 12:26:03 -0400

    updated delete handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -9,81 +9,81 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// NOTE: - Handlers file should show handlers first
+
 func ShowHomePage(writer http.ResponseWriter, req *http.Request) {
 	http.ServeFile(writer, req, "./static/static.html")
 }
 
 func GetAllCustomers(writer http.ResponseWriter, req *http.Request) {
-	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(http.StatusOK)
 	customers := customer.GetAll()
-	json.NewEncoder(writer).Encode(customers)
+	writeResponse(writer, customers, http.StatusOK)
 }
 
 func GetSingleCustomer(writer http.ResponseWriter, req *http.Request) {
 	// Handler logic
-	writer.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(req)
-	id := params["id"]
+	id := extractID(req)
 
 	customer, err := customer.Get(id)
 
 	if err != nil {
-		writer.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(writer).Encode(customer)
+		writeResponse(writer, customer, http.StatusNotFound)
 		return
 	}
-
-	writer.WriteHeader(http.StatusAccepted)
-	json.NewEncoder(writer).Encode(customer)
+	writeResponse(writer, customer, http.StatusOK)
 }
 
+// NOTE: - Keep helpers at the bottom of the page
+func writeResponse(writer http.ResponseWriter, data any, statusCode int) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+	json.NewEncoder(writer).Encode(data)
+}
+
+func extractID(req *http.Request) string {
+	params := mux.Vars(req)
+	id := params["id"]
+	return id
+}
+
+//	func CreateNewCustomer(writer http.ResponseWriter, req *http.Request) {
+//		// 1. set content-type to JSON
+//		writer.Header().Set("Content-Type", "application/json")
 //
-//func CreateNewCustomer(writer http.ResponseWriter, req *http.Request) {
-//	// 1. set content-type to JSON
-//	writer.Header().Set("Content-Type", "application/json")
+//		// 2. keep track of new entry so that it can be added to dictionary map
+//		var newEntry map[string]models.Customer
 //
-//	// 2. keep track of new entry so that it can be added to dictionary map
-//	var newEntry map[string]models.Customer
+//		// 3. Read the request
+//		reqBody, _ := io.ReadAll(req.Body)
 //
-//	// 3. Read the request
-//	reqBody, _ := io.ReadAll(req.Body)
+//		// 4. Parse JSON Body
+//		json.Unmarshal(reqBody, &newEntry)
 //
-//	// 4. Parse JSON Body
-//	json.Unmarshal(reqBody, &newEntry)
-//
-//	// 5. Add new entry to dictionary map if it doesn't already exist
-//	for key, value := range newEntry {
-//		// - Respond with conflict if entry exists
-//		if _, ok := database[key]; ok {
-//			writer.WriteHeader(http.StatusConflict)
-//		} else {
-//			// - Respond with OK if entry does not exist
-//			database[key] = value
-//			writer.WriteHeader(http.StatusCreated)
+//		// 5. Add new entry to dictionary map if it doesn't already exist
+//		for key, value := range newEntry {
+//			// - Respond with conflict if entry exists
+//			if _, ok := database[key]; ok {
+//				writer.WriteHeader(http.StatusConflict)
+//			} else {
+//				// - Respond with OK if entry does not exist
+//				database[key] = value
+//				writer.WriteHeader(http.StatusCreated)
+//			}
 //		}
-//	}
 //
-//	// 6. Return updated dictionary
-//	json.NewEncoder(writer).Encode(database)
-//}
-//
-//func DeleteCustomer(writer http.ResponseWriter, req *http.Request) {
-//	// 1. Set Content Type
-//	writer.Header().Set("Content-Type", "application/json")
-//	// 2. Grab the member id from the url params
-//	params := mux.Vars(req)
-//	id := params["id"]
-//	if _, ok := database[id]; ok {
-//		// delete the entry, return successful response
-//		delete(database, id)
-//		writer.WriteHeader(http.StatusAccepted)
-//		json.NewEncoder(writer).Encode(database)
-//	} else { // 4. If not, return an error, but still return the dictionary
-//		writer.WriteHeader(http.StatusNotFound)
+//		// 6. Return updated dictionary
 //		json.NewEncoder(writer).Encode(database)
 //	}
-//}
+func DeleteCustomer(writer http.ResponseWriter, req *http.Request) {
+	id, database := extractID(req), customer.GetAll()
+	if _, ok := database[id]; ok {
+		delete(database, id)
+		writeResponse(writer, database, http.StatusNoContent)
+		return
+	}
+	writeResponse(writer, database, http.StatusNotFound)
+}
+
 //
 //// TODO: - update a customer by id
 //func UpdateCustomer(writer http.ResponseWriter, req *http.Request) {
@@ -91,12 +91,13 @@ func GetSingleCustomer(writer http.ResponseWriter, req *http.Request) {
 //	params := mux.Vars(req)
 //	id := params["id"]
 //
-//	var newEntry map[string]models.Customer
+//	var newEntry models.Customer
 //	if _, ok := database[id]; ok {
 //		reqBody, _ := io.ReadAll(req.Body)
 //		json.Unmarshal(reqBody, &newEntry)
-//		value := newEntry[id]
-//		database[id] = value
+
+//		database[newEntry.id] = newEntry
+//		database[newEntry.id].Name = newEntry.Name
 //		writer.WriteHeader(http.StatusAccepted)
 //		json.NewEncoder(writer).Encode(database)
 //	} else {
--- models/customer.go
+++ models/customer.go
@@ -1,10 +1,16 @@
 package models
 
 type Customer struct {
-	ID        string
-	Name      string
-	Role      string
+	ID        string // can't be empty
+	Name      string // can't be empty
+	Role      string `json: "role"`
 	Email     string
 	Phone     string
-	Contacted bool
+	Contacted bool // 0'th value is false
 }
+
+var myVar string            //emptyString (0'th value)
+var myMap map[string]string // nil
+var myMap2 map[int]int      // nil
+
+// TODO: - What here needs to be validate?  i.e name can't be empty.  what can and cannot be empty?
